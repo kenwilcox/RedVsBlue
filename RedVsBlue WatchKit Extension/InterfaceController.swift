54af5effa434416b74c6264ac0364591eb57092e
Ken Wilcox <wilcoxk@gmail.com>
2015-03-28 20:58:17 -0600

    initial UI setup

--- RedVsBlue WatchKit Extension/InterfaceController.swift
+++ RedVsBlue WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,32 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
+  
+  @IBOutlet weak var group: WKInterfaceGroup!
+  @IBOutlet weak var label: WKInterfaceLabel!
+  @IBOutlet weak var slider: WKInterfaceSlider!
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+    group.setBackgroundColor(UIColor.blueColor())
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  @IBAction func switchPressed(value: Bool) {
+    
+  }
+  @IBAction func resetPressed() {
+    
+  }
 }
